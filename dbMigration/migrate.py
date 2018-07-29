4d881090f37a48b048de140edf55f1b90be1eb99
Reynolds534 <dreynolds@thorntontomasetti.com>
2018-07-28 23:52:40 -0400

    db Migration

--- dbMigration/migrate.py
+++ dbMigration/migrate.py
@@ -1,22 +1,26 @@
 import csv
-import os
+import os, sys
+from flask import jsonify
+sys.path.insert(0, os.path.abspath(".."))
+from pymongo import MongoClient
 
-# Define class for the mongo object
-class ImageMetadata(object):
 
-    def __init__(self, Id, Name, PinterestUrl, StructuralEmphasis, Slenderness, Symmetry, Repetition, Complexity, Sequence, PinterestSection):
-        self.Id = Id
-        self.Name = Name
-        self.PinterestUrl = PinterestUrl
-        self.StructuralEmphasis = StructuralEmphasis
-        self.Slenderness = Slenderness
-        self.Symmetry = Symmetry
-        self.Repetition = Repetition
-        self.Complexity = Complexity
-        self.Sequence = Sequence
-        self.PinterestSection = PinterestSection
+import Schema.taste_test as TasteTestSchemata
 
 if __name__ == '__main__':
+
+    image_metadata_schema = TasteTestSchemata.ImageMetadataSchema()
+
+    DB_NAME = str(os.environ.get('TASTE_DB_NAME'))
+    DB_HOST = str(os.environ.get('TASTE_DB_HOST'))
+    DB_PORT = int(os.environ.get('TASTE_DB_PORT'))
+    DB_USER = str(os.environ.get('TASTE_DB_USER'))
+    DB_PASS = str(os.environ.get('TASTE_DB_PASS'))
+    
+    connection = MongoClient(DB_HOST, DB_PORT)
+    db = connection[DB_NAME]
+    db.authenticate(DB_USER, DB_PASS)
+    image_data = db.image_data
 
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
@@ -24,5 +28,11 @@ if __name__ == '__main__':
     with open(os.path.join(__location__,'taste_test_images.csv'), 'rb') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in spamreader:
-            data = ImageMetadata(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9])
-            print data.Name
+            mod = []
+            for val in row:
+                mod.append(val.replace('"', ''))
+            row=mod
+            data = TasteTestSchemata.ImageMetadata(row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9])
+            post_data, errors = image_metadata_schema.dump(data)
+            result = image_data.insert_one(post_data)
+            print('One post: {0}'.format(result.inserted_id))
